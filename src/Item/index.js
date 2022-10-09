9e62794297dc021e7235dab21fd1ce9cd008174d
Prawito Hudoro <prawitohudoro@gmail.com>
2022-10-10 05:42:23 +0700

    update static page

--- src/Item/index.js
+++ src/Item/index.js
@@ -2,15 +2,14 @@ import currency from "../utils/currency";
 import Counter from "./Counter";
 
 function Item(props){
-    const {item: {name, img, price}} = props;
+    const {item: {name, img, price, value}} = props;
     return(
       <div className='item'>
         <img src={img} alt='item-1' />
         <div className='item-info'>
           <p className='item-name'>{name}</p>
           <p className='item-price'>{currency(price)}</p>
-          <button className='btn-add'>Add</button>
-          <Counter />
+          {value > 0 ? <Counter /> : <button className='btn-add'>Add</button>}
         </div>
       </div>
     )
