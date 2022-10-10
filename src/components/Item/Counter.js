f275957f5109ab375ca8533605c64fcce33bc97d
Prawito Hudoro <prawitohudoro@gmail.com>
2022-10-11 06:15:43 +0700

    Membuat Fungsi Counter

--- src/components/Item/Counter.js
+++ src/components/Item/Counter.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 function Counter(){
+    const [value, setValue] = useState(0);
+
     return(
         <div className='counter'>
-            <button>-</button>
-            <input type="number" />
-            <button>+</button>
+            <button onClick={() => setValue(value > 0 ? value - 1 : value)}>-</button>
+            <input type="number" value={value} />
+            <button onClick={() => setValue(value + 1)}>+</button>
         </div>
     )
 }
